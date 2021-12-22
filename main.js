a3f3e80014bd8e6b59efaf646d282f88d1d3ba9f
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2021-12-22 15:47:38 +0000

    function built

--- main.js
+++ main.js
@@ -4,4 +4,23 @@
 
 //👉 Write the function your CodeWarriors will start with below here:
 
-export function toKebabCase(string) {}
+/* 
+👉 Write your kata here!
+*/
+export function toKebabCase(string) {
+  const letters = string.split('');
+  let index = 0;
+  let letter = '';
+
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] === letters[i].toUpperCase()) {
+      index = [i];
+      letter = letters[i].toLowerCase();
+      letters.splice(index, 1, `-${letter}`);
+    }
+  }
+
+  return letters.join('');
+}
+//👉 Write the function your CodeWarriors will start with below here:
+
